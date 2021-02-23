b091184d43ffa5ee4e5f553b7cd6d2194b06fa58
Csaba Hegedűs <csaba.hegedus@bitknights.com>
2021-02-23 16:03:42 +0100

    # Aesthetic modification for the rate.

--- trading-app/trading-app/data/network/Responses.swift
+++ trading-app/trading-app/data/network/Responses.swift
@@ -37,11 +37,17 @@ struct ResponseData: Codable, Hashable, Equatable {
 
 extension ResponseData {
     var rate: String {
+        if (lastPrice == 0) {
+            return " - "
+        }
         let rate = price - lastPrice
         return (rate >= 0) ? "+\(rate.rounded(toPlaces: 2))" : "\(rate.rounded(toPlaces: 2))"
     }
     
     var rateColor: UIColor {
+        if (lastPrice == 0) {
+            return UIColor.systemGray
+        }
         return ((price - lastPrice) >= 0) ? UIColor.systemGreen : UIColor.systemRed
     }
 }
